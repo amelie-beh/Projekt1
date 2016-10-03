df24b0d539d8b58fab3152606e7d6756cd24cb30
amelie-beh <amelie-behrendt@web.de>
2016-10-03 17:20:00 +0200

    Doppelte Nennung der Aktionen gelöscht

--- view_spielregeln.php
+++ view_spielregeln.php
@@ -26,15 +26,15 @@
 				
 				<h2>Spielablauf:</h2>
 
-				<p>Ein Spieler kann aus 4 verschiedenen Aktionen wählen: eine Forscherkarte nehmen, die Kartenauslage austauschen (nur in Warschau möglich), eine Ausgrabung durchführen oder eine Ausstellung durchführen.
-				<p> - Eine Forscherkarte nehmen:</p>
-				<p> -> Der Spieler kann zwischen 4 verschiedenen Forscherkarten wählen, die ihm diverse Vorteile verschaffen. Um eine Forscherkarte zu nehmen, muss sich der Spieler in der jeweiligen Stadt befinden. Das heißt der Spieler reist zu dem Ort und kann sich dann diese Karte nehmen. Die Zeit von der Reise wird mit der Zeit für die Forschung addiert und dem Spieler zu seiner Zeitleiste hinzugefügt.
-				<p> - Die Kartenauslage tauschen:</p>
-				<p> -> Diese Aktion ist nur in Warschau möglich. Hierbei werden alle Forscherkarten und Ausstellungskarten ausgetauscht.
-				<p> - Eine Ausgrabung durchführen:</p>
-				<p> -> Der Spieler reist zum Ausgrabungsort und muss sich entscheiden, wie lange er an diesem Ort graben möchte. Dafür ist es wichtig, wie viel Spezialwissenskarten er vorher bekommen hat. Denn je mehr Spezialwissen er hat, desto höher der Grabungserfolg. Da der Spieler an diesem Ort gegraben hat, verfällt seine Grabungserlabnis für dieses Jahr. Erst im nächsten Jahr darf er eine neue Grabung und diesem Ort ausführen.</p>
-				<p> - Eine Ausstellung durchführen:</p>
-				<p> -> Um eine Ausstellung durchzuführen, muss der Spieler gewisse Fundstücke besitzen. Diese sind durch Farben markiert. Hat der Spieler diese Fundstücke, erhält er die Ausstellungskarte und somit die Ruhmespunkte. Die Fundstücke behält der Spieler. </p>
+				<p>Ein Spieler kann aus 4 verschiedenen Aktionen wählen: 
+				<p> Eine Forscherkarte nehmen:</p>
+				<p> - Der Spieler kann zwischen 4 verschiedenen Forscherkarten wählen, die ihm diverse Vorteile verschaffen. Um eine Forscherkarte zu nehmen, muss sich der Spieler in der jeweiligen Stadt befinden. Das heißt der Spieler reist zu dem Ort und kann sich dann diese Karte nehmen. Die Zeit von der Reise wird mit der Zeit für die Forschung addiert und dem Spieler zu seiner Zeitleiste hinzugefügt.
+				<p> Die Kartenauslage tauschen:</p>
+				<p> - Diese Aktion ist nur in Warschau möglich. Hierbei werden alle Forscherkarten und Ausstellungskarten ausgetauscht.
+				<p> Eine Ausgrabung durchführen:</p>
+				<p> -Der Spieler reist zum Ausgrabungsort und muss sich entscheiden, wie lange er an diesem Ort graben möchte. Dafür ist es wichtig, wie viel Spezialwissenskarten er vorher bekommen hat. Denn je mehr Spezialwissen er hat, desto höher der Grabungserfolg. Da der Spieler an diesem Ort gegraben hat, verfällt seine Grabungserlabnis für dieses Jahr. Erst im nächsten Jahr darf er eine neue Grabung und diesem Ort ausführen.</p>
+				<p> Eine Ausstellung durchführen:</p>
+				<p> - Um eine Ausstellung durchzuführen, muss der Spieler gewisse Fundstücke besitzen. Diese sind durch Farben markiert. Hat der Spieler diese Fundstücke, erhält er die Ausstellungskarte und somit die Ruhmespunkte. Die Fundstücke behält der Spieler. </p>
 				<p>Die Reihenfolge der Züge wird durch die Zeit bestimmt. Der Spieler, der zeitlich am weitesten zurück liegt, hat den nächsten Zug.</p>
 				<p>Beginnt für einen Spieler ein neues Jahr, dann werden alle Grabungserlaubis wieder aktiv.</p>
 
