4a15bf5c729f00c48b965a6761cd2c20a7a793c6
amelie-beh <amelie-behrendt@web.de>
2016-10-15 16:25:48 +0200

    account_erstellen an strukt.html angepasst, funktion newaccount zur to do liste hinzugefügt

--- Gruppen_de/account_erstellen.php
+++ Gruppen_de/account_erstellen.php
@@ -2,19 +2,19 @@
 	- - - - - - - - - - - - - - - - -
 	- - - XI. Account erstellen - - -
 	- - - - - - - - - - - - - - - - -
-	-71- Überschrift ("Account erstellen")
+	-71- h1 Überschrift / Account erstellen
+	-11- Formular
 	-74- Absatz
-	-01- Label (für Username)
+	-01- Label / Username:
 	-74- Absatz
-	-02- Label (für Passwort)
+	-02- Label / Passwort:
 	-74- Absatz
-	-38- Label (Passwort bestätigen)
+	-38- Label / Passwort bestätigen:
 	-74- Absatz
-	-37- Label (Email Adresse eingeben)
+	-37- Label / Email Adresse eingeben:
+	-15- Formular / Action ruft PHP Funktion auf
 	-74- Absatz
-	-03- Schaltfläche (Bestätigen, Link zu view_startseite)
-	-74- Absatz
-	-05- Link (Anzeige Spielregeln, Link zu view_spielregeln)
+	-03- Input / Submit
 -->
 <h1>Account erstellen</h1>
 <!--Formular muss Eintragung in Datenbank für diesen Spieler übernehmen-->
@@ -25,8 +25,6 @@
 		<p><label for="email">EMail: <input id=email name=email> </label></p>
 	</form>
 
-	<form action=view_startseite.php target=_self>
+	<form action=function_newaccount.php>
 		<p><input type=submit value=Bestätigen></p>
 	</form>
-
-	<p><a href=view_spielregeln.php>Spielregeln</a></p>
